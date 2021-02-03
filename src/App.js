1153629405a6362894b402ff526752c26d41a67c
Nahid Karim Ankur <nahidankur@gmail.com>
2021-02-04 02:37:46 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,48 +1,3 @@
-// import React, {useState, useEffect} from 'react'
-// import ImageCard from './components/ImageCard'
-// import ImageSearch from './components/ImageSerach'
-
-// const App = ()=>{
-
-//     const [images, setImages] = useState([])
-//     const [isLoading, setIsLoading] = useState(true)
-//     const [term, setTerm] = useState('')
-
-//     useEffect(()=>{
-//         fetch(`https://pixabay.com/api/?key=20139365-8604d003e71feba235bb75894&q=${term}&image_type=photo&pretty=true`)
-//         .then(res => res.json())
-//         .then(data => {
-//             setImages(data.hits)
-//             setIsLoading(false)
-//         })
-//         .catch(err => console.log(err))
-//     },[term])
-//     return(
-//        <div className='container mx-auto'>
-//            <ImageSearch searchText={(text)=> setTerm(text)} />
- 
-//            {!isLoading && images.length ===0 && <h1 className='text-5xl text-center mx-auto'>No Image Found</h1> }
-
-//            {isLoading ? <h1 className='text-6xl text-center mx-auto'>Loading...</h1>: <div className='grid grid-cols-3 gap-4'>
-//              {images.map((image)=>(
-//                  <ImageCard key={image.id} image={image} />
-//              ))}
-//            </div>}
-
-           
-//        </div>
-//     )
-// }
-//  export default App
-
-
-
-
-
-
-
-
-
 import React, {useState, useEffect} from 'react'
 import ImageCard from './components/ImageCard'
 import ImageSearch from './components/ImageSerach'
@@ -54,7 +9,7 @@ const App = ()=>{
     const [term, setTerm] = useState('')
 
     useEffect(()=>{
-        fetch(`https://pixabay.com/api/?key=20139365-8604d003e71feba235bb75894&q=${term}&image_type=photo&pretty=true`)
+        fetch(`https://pixabay.com/api/?key=YOUR_PIXABAY_API_KEY_HERE&q=${term}&image_type=photo&pretty=true`)
         .then(res => res.json())
         .then(data => {
             setImages(data.hits)
